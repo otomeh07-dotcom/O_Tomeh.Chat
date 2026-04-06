0a51a2b43848225b972ff2057d989af2a56af2ee
otomeh07-dotcom <otomeh07@gmail.com>
2026-04-06 19:45:05 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,11 @@
 export const appConfig = {
   appName: "O_Tomeh.Chat",
-publicBaseUrl: "https://otomeh07-dotcom.github.io/O_Tomeh.Chat/",
+  publicBaseUrl: "https://otomeh07-dotcom.github.io/O_Tomeh.Chat/",
   supabaseUrl: "https://wcjvhjxixzrkpxqnvgvh.supabase.co",
   supabaseAnonKey:
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndjanZoanhpeHpya3B4cW52Z3ZoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUyOTYzNjIsImV4cCI6MjA5MDg3MjM2Mn0.I-5JxDzfsMmpOlUSjk35cDlZ52bM5MBLSzUhT83bhDo",
+
+  turnUrl: "turn:YOUR_TURN_HOST:3478",
+  turnUsername: "YOUR_TURN_USERNAME",
+  turnCredential: "YOUR_TURN_PASSWORD",
 };
